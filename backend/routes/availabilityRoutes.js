f8ab7826bed6229cee348965d0a028b115b6cea3
MKRCabanero <mkrcabanero.qut@gmail.com>
2025-10-01 19:42:49 +1000

    SIM-63 updated frontend

--- backend/routes/availabilityRoutes.js
+++ backend/routes/availabilityRoutes.js
@@ -5,7 +5,7 @@ const validateAvailability = require("../middleware/validators/validateAvailabil
 // const { protect} = require('../middleware/authMiddleware');
 
 // Create a new availability
-router.post("/", protect, validateAvailability, AvailabilityController.create);
+router.post("/",  validateAvailability, AvailabilityController.create);
 
 // Get all availabilities
 router.get("/", AvailabilityController.getAll);
@@ -14,9 +14,9 @@ router.get("/", AvailabilityController.getAll);
 router.get("/:id", AvailabilityController.getById);
 
 // Update a availabilty by ID
-router.put("/:id", protect, validateAvailability, AvailabilityController.update);
+router.patch("/:id", validateAvailability, AvailabilityController.update);
 
 // Delete a availability by ID
-router.delete("/:id", protect, AvailabilityController.delete);
+router.delete("/:id", AvailabilityController.delete);
 
 module.exports = router;
